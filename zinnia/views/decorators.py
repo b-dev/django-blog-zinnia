4b036c43eaf7e0c73a68a951d52c15b3cfb95381
marco <marco@marco-laptop.(none)>
2012-03-24 18:10:33 +0100

    aggiunta gestione url per categorie

--- zinnia/views/decorators.py
+++ zinnia/views/decorators.py
@@ -67,6 +67,31 @@ def protect_entry(view):
     return wrapper
 
 
+def protect_category_entry(view):
+    """Decorator performing a security check if needed
+    around the generic.date_based.entry_detail view
+    and specify the template used to render the entry"""
+
+    @wraps(view)
+    def wrapper(*args, **kwargs):
+        """Do security check and retrieve the template"""
+        request = args[0]
+        categoria = kwargs['categoria']
+        del kwargs['categoria']
+        entry = get_object_or_404(kwargs['queryset'], slug=kwargs['slug'],
+                                  categories__slug=categoria)
+
+
+        if entry.login_required and not request.user.is_authenticated():
+            return login(request, 'zinnia/login.html')
+        if entry.password and entry.password != \
+               request.session.get('zinnia_entry_%s_password' % entry.pk):
+            return password(request, entry)
+        kwargs['template_name'] = entry.template
+        return view(*args, **kwargs)
+    return wrapper
+
+
 def template_name_for_entry_queryset_filtered(model_type, model_name):
     """Return a custom template name for views
     returning a queryset of Entry filtered by another model."""
--- zinnia/views/entries.py
+++ zinnia/views/entries.py
@@ -1,7 +1,9 @@
 """Views for Zinnia entries"""
-from django.views.generic.date_based import object_detail
+from django.views.generic.date_based import object_detail as date_object_detail
+from django.views.generic.list_detail import object_detail
 
-from zinnia.views.decorators import protect_entry
+from zinnia.views.decorators import protect_entry, protect_category_entry
 
 
-entry_detail = protect_entry(object_detail)
+entry_category_detail = protect_category_entry(object_detail)
+entry_detail = protect_entry(date_object_detail)
